31ae17e1ac8d8585ad2605692794f23711a998dd
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-10-19 16:39:27 -0400

    More cleanup

--- js/main.js
+++ js/main.js
@@ -16,10 +16,10 @@ var BulletVotes = BulletVotes || {};
     document.getElementById('ward-votes-header').innerHTML = output;
   };
 
-  NS.setWardCandidatesHeader = function(tcontext) {
-    var tpl = document.getElementById('ward-candidates-header-tpl').innerHTML;
+  NS.setWardBulletsHeader = function(tcontext) {
+    var tpl = document.getElementById('ward-bullets-header-tpl').innerHTML;
     var output = Mustache.render(tpl, tcontext);
-    document.getElementById('ward-candidates-header').innerHTML = output;
+    document.getElementById('ward-bullets-header').innerHTML = output;
   };
 
   NS.setDivisionVotesHeader = function(tcontext) {
@@ -36,14 +36,16 @@ var BulletVotes = BulletVotes || {};
 
   // NOTE: Hard-coding these cadidate values is not ideal, but is easiest for
   // now.
-  NS.bulletFieldNames = {
-    'neilson': 'Neilson', 'rizzo': 'Rizzo', 'wyatt': 'Wyatt', 'gym': 'Gym',
-    'domb': 'Domb', 'green': 'Green', 'steinke': 'Steinke',
-    'reynolds_brown': 'Reynolds Brown', 'goode': 'Goode',
-    'aument_loughrey': 'Aument-Loughrey', 'cohen': 'Cohen',
-    'alexander': 'Alexander', 'thomas': 'Thomas', 'greenlee': 'Greenlee',
-    'cain': 'Cain', 'ayers': 'Ayers', 'write_in': 'Write in', 'other': 'Other'
-  };
+  NS.bulletFieldNames = [
+    'neilson', 'rizzo', 'wyatt', 'gym', 'domb', 'green', 'steinke',
+    'reynolds_brown', 'goode', 'aument_loughrey', 'cohen', 'alexander',
+    'thomas', 'greenlee', 'cain', 'ayers', 'write_in', 'other'
+  ];
+  NS.bulletFieldLabels = _.object(NS.bulletFieldNames, [
+    'Neilson', 'Rizzo', 'Wyatt', 'Gym', 'Domb', 'Green', 'Steinke',
+    'Reynolds Brown', 'Goode', 'Aument-Loughrey', 'Cohen', 'Alexander',
+    'Thomas', 'Greenlee', 'Cain', 'Ayers', 'Write in', 'Other'
+  ]);
 
   NS.initWardVotesChart = function() {
     NS.wardVotesChart = c3.generate({
@@ -89,13 +91,13 @@ var BulletVotes = BulletVotes || {};
 
   NS.initWardBulletsChart = function() {
     NS.wardBulletsChart = c3.generate({
-      bindto: '#ward-candidates-chart-wrapper',
+      bindto: '#ward-bullets-chart-wrapper',
       data: {
         type: 'donut',
         columns: BulletVotes.bulletFieldNames.map(function(fieldname) {
           return [fieldname, 0];
         }).concat([['other', 0]]),
-        names: BulletVotes.bulletFieldNames,
+        names: BulletVotes.bulletFieldLabels,
         order: null
       },
       donut: {
@@ -140,71 +142,80 @@ var BulletVotes = BulletVotes || {};
     });
   };
 
-  function main() {
-    BulletVotes.initWardVotesChart();
-    BulletVotes.initWardBulletsChart();
+  NS.goToWard = function(ward) {
+    var sql = new cartodb.SQL({ user: 'mjumbewu' });
+    sql.execute("SELECT * FROM political_wards_merge WHERE ward = {{ward}}", {'ward': ward})
+      .done(function(data) {
+        var d = data.rows[0];
 
-    // add link to CartoDB viz.json here
-    cartodb.createVis('map', 'https://mjumbewu.cartodb.com/api/v2/viz/416a0320-757a-11e5-8ba1-0e3ff518bd15/viz.json', {
-        shareable: false ,
-        title: false,
-        description: false,
-        search: true,
-        tiles_loader: true,
-        center_lat: 39.9894197,
-        center_lon: -75.1214633,
-        zoom: 11,
-        cartodb_logo: false
-    })
-    .done(function(vis, layers  ) {
-      // layer 0 is the base layer, layer 1 is cartodb layer
-      // setInteraction is disabled by default
-      layers[1].setInteraction(true);
-      layers[1].on('featureClick', function(e, pos, latlng, data) {
-        cartodb.log.log(e, pos, latlng, data);
+        BulletVotes.setWardVotesHeader(d);
+        BulletVotes.setWardBulletsHeader(d);
+        BulletVotes.setDivisionVotesHeader(d);
+        BulletVotes.setDivisionCandidatesHeader(d);
 
-        var sql = new cartodb.SQL({ user: 'mjumbewu' });
-        sql.execute("SELECT * FROM political_wards_merge WHERE cartodb_id = {{cartodb_id}}", data)
-          .done(function(data) {
-            var d = data.rows[0];
-
-            BulletVotes.setWardVotesHeader(d);
-            BulletVotes.setWardCandidatesHeader(d);
-            BulletVotes.setDivisionVotesHeader(d);
-            BulletVotes.setDivisionCandidatesHeader(d);
-
-            BulletVotes.updateWardVotesChart([
-              d['_0_votes'], d['_1_votes'], d['_2_votes'],
-              d['_3_votes'], d['_4_votes'], d['_5_votes']
-            ]);
-            BulletVotes.updateWardBulletsChart(
-              _(BulletVotes.bulletFieldNames).keys().map(function(fieldname) {
-                return [fieldname, d[fieldname]];
-              })
-            );
-
-            BulletVotes.hideIntro();
-            BulletVotes.showStats();
-
-            console.log(data.rows);
+        BulletVotes.updateWardVotesChart([
+          d['_0_votes'], d['_1_votes'], d['_2_votes'],
+          d['_3_votes'], d['_4_votes'], d['_5_votes']
+        ]);
+        BulletVotes.updateWardBulletsChart(
+          _(BulletVotes.bulletFieldNames).without('other').map(function(fieldname) {
+            return [fieldname, d[fieldname]];
           })
-          .error(function(errors) {
-            // errors contains a list of errors
-            console.log("errors:" + errors);
-          });
+        );
+
+        BulletVotes.hideIntro();
+        BulletVotes.showStats();
+
+        console.log(data.rows);
+      })
+      .error(function(errors) {
+        // errors contains a list of errors
+        console.log("errors:" + errors);
       });
-      // you can get the native map to work with it
-      // depending if you use google maps or leaflet
-      map = vis.getNativeMap();
-      // now, perform any operations you need
-      // map.setZoom(3)
-      // map.setCenter(new google.maps.Latlng(...))
-    })
-    .error(function(err) {
-      console.log(err);
-    });
-  }
-
-  window.onload = main;
-
+  };
 })(BulletVotes);
+
+
+function canPushState() {
+  return (window.history && window.history.pushState && window.history.replaceState ? true : false);
+}
+
+function main() {
+  BulletVotes.initWardVotesChart();
+  BulletVotes.initWardBulletsChart();
+
+  // add link to CartoDB viz.json here
+  cartodb.createVis('map', 'https://mjumbewu.cartodb.com/api/v2/viz/416a0320-757a-11e5-8ba1-0e3ff518bd15/viz.json', {
+      shareable: false ,
+      title: false,
+      description: false,
+      search: true,
+      tiles_loader: true,
+      center_lat: 39.9894197,
+      center_lon: -75.1214633,
+      zoom: 11,
+      cartodb_logo: false
+  })
+  .done(function(vis, layers) {
+    // layer 0 is the base layer, layer 1 is cartodb layer
+    // setInteraction is disabled by default
+    layers[1].setInteraction(true);
+    layers[1].on('featureClick', function(e, pos, latlng, data) {
+      cartodb.log.log(e, pos, latlng, data);
+      BulletVotes.goToWard(data.ward);
+
+    });
+    // you can get the native map to work with it
+    // depending if you use google maps or leaflet
+    map = vis.getNativeMap();
+    // now, perform any operations you need
+    // map.setZoom(3)
+    // map.setCenter(new google.maps.Latlng(...))
+  })
+  .error(function(err) {
+    console.log(err);
+  });
+}
+
+window.onload = main;
+

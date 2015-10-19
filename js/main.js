d50f4550775cbcc3068d8fe080d61635eed9818d
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-10-19 10:23:11 -0400

    Initial commit
    * Started from the JavaScript code from @laurenancona's [health dashboard](https://github.com/laurenancona/health-dashboard)
    * Wrapped in the phila.gov [informational page boilerplate](http://cityofphiladelphia.github.io/patterns/templates/information-page/)
    * Using [C3.js](http://c3js.org/) for charting and [Mustache.js](https://mustache.github.io/) for text templating
    * Using data from OpenDataPhilly.org for political wards in Philadelphia. Joining this data with data from the commissioner's office about city council votes from the 2015 primary election

---
+++ js/main.js
@@ -0,0 +1,110 @@
+var BulletVotes = BulletVotes || {};
+
+(function(NS) {
+
+  NS.hideIntro = function() {
+    document.getElementById('intro').classList.add('hide');
+  };
+
+  NS.showStats = function() {
+    document.getElementById('stats').classList.remove('hide');
+  };
+
+  NS.setWardVotesHeader = function(tcontext) {
+    var tpl = document.getElementById('ward-votes-header-tpl').innerHTML;
+    var output = Mustache.render(tpl, tcontext);
+    document.getElementById('ward-votes-header').innerHTML = output;
+  };
+
+  NS.setWardCandidatesHeader = function(tcontext) {
+    var tpl = document.getElementById('ward-candidates-header-tpl').innerHTML;
+    var output = Mustache.render(tpl, tcontext);
+    document.getElementById('ward-candidates-header').innerHTML = output;
+  };
+
+  NS.initWardVotesChart = function() {
+    NS.wardVotesChart = c3.generate({
+        bindto: '#ward-votes-chart-wrapper',
+        data: {
+            columns: [
+                ['# of Candidates Chosen', 0, 0, 0, 0, 0, 0]
+            ],
+            type: 'bar'
+        },
+        bar: {
+            width: {
+                ratio: 0.85 // this makes bar width 50% of length between ticks
+            }
+            // or
+            //width: 100 // this makes bar width 100px
+        }
+    });
+  };
+
+  NS.updateWardVotesChart = function(values) {
+    NS.wardVotesChart.load({
+      columns: [
+        ['# of Candidates Chosen'].concat(values)
+      ]
+    });
+  };
+
+  function main() {
+    BulletVotes.initWardVotesChart();
+
+    // add link to CartoDB viz.json here
+    cartodb.createVis('map', 'https://mjumbewu.cartodb.com/api/v2/viz/416a0320-757a-11e5-8ba1-0e3ff518bd15/viz.json', {
+        shareable: false ,
+        title: false,
+        description: false,
+        search: true,
+        tiles_loader: true,
+        center_lat: 39.9894197,
+        center_lon: -75.1214633,
+        zoom: 11,
+        cartodb_logo: false
+    })
+    .done(function(vis, layers  ) {
+      // layer 0 is the base layer, layer 1 is cartodb layer
+      // setInteraction is disabled by default
+      layers[1].setInteraction(true);
+      layers[1].on('featureClick', function(e, pos, latlng, data) {
+        cartodb.log.log(e, pos, latlng, data);
+
+        var sql = new cartodb.SQL({ user: 'mjumbewu' });
+        sql.execute("SELECT * FROM political_wards_merge WHERE cartodb_id = {{cartodb_id}}", data)
+          .done(function(data) {
+            var d = data.rows[0];
+
+            BulletVotes.setWardVotesHeader(d);
+            BulletVotes.setWardCandidatesHeader(d);
+            BulletVotes.updateWardVotesChart([
+              d['_0_votes'], d['_1_votes'], d['_2_votes'],
+              d['_3_votes'], d['_4_votes'], d['_5_votes']
+            ]);
+
+            BulletVotes.hideIntro();
+            BulletVotes.showStats();
+
+            console.log(data.rows);
+          })
+          .error(function(errors) {
+            // errors contains a list of errors
+            console.log("errors:" + errors);
+          });
+      });
+      // you can get the native map to work with it
+      // depending if you use google maps or leaflet
+      map = vis.getNativeMap();
+      // now, perform any operations you need
+      // map.setZoom(3)
+      // map.setCenter(new google.maps.Latlng(...))
+    })
+    .error(function(err) {
+      console.log(err);
+    });
+  }
+
+  window.onload = main;
+
+})(BulletVotes);
---
+++ processwards.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+import csv
+import click
+import json
+import sys
+from os.path import join as pathjoin, dirname, abspath
+
+
+ROOT = abspath(dirname(__file__))
+
+
+def finalizeward(wardnum, wardrow, fieldnames, bulletindex, doubleindex, writer, wards, outfields):
+    wardrow.insert(0, wardnum)
+    cleanward = '{:0>2}'.format(wardnum)
+
+    # Calculate the top bullet and pair getters
+    topbulletcount = max(wardrow[bulletindex:doubleindex])
+    topbulletindex = wardrow.index(topbulletcount, bulletindex, doubleindex)
+    topbullet = fieldnames[topbulletindex]
+    topdoublecount = max(wardrow[doubleindex:])
+    topdoubleindex = wardrow.index(topdoublecount, doubleindex)
+    topdouble = fieldnames[topdoubleindex]
+
+    # Output each ward's row as it's claculated.
+    outrow = ([wardnum, cleanward] +
+              [topbullet, topbulletcount] +
+              [topdouble, topdoublecount] + wardrow[1:])
+    writer.writerow(outrow)
+
+    feature = {'type': 'Feature',
+               'properties': dict(zip(outfields, outrow)),
+               'geometry': wards[str(wardnum)]['geometry']}
+    return feature
+
+
+@click.command()
+@click.argument('files', nargs=-1, type=click.Path())
+def process_csvs(files):
+    for filename in files:
+        with open(pathjoin(ROOT, 'data', 'GIS_PLANNING.Political_Wards.geojson')) as jsonfile:
+            # Index the wards by ward number
+            wards_geojson = json.load(jsonfile)
+            wards = {
+                str(feature['properties']['WARD_NUM']): feature
+                for feature in wards_geojson['features']
+            }
+
+        with open(filename, 'rU') as csvfile:
+            # Start reading in as a CSV file
+            reader = csv.reader(csvfile)
+
+            # Get rid of the first row, but remember where the pairs start
+            legend = next(reader)
+            bulletindex = legend.index('Bullet Votes Per Candidate')
+            doubleindex = legend.index('Double Bullet Pairs')
+
+            # Read and fix the header
+            fieldnames = list(next(reader))
+            fieldnames[1:7] = ['{} Votes'.format(v)
+                               for v in range(0, 6)]
+
+            # Clean up the headers for pair candidates
+            i = doubleindex
+            while i < len(fieldnames):
+                fieldnames[i] = fieldnames[i].replace(' ', ' & ')
+                i = i + 1
+
+            # Print the header row
+            outfields = (['Ward', 'Ward Display'] +
+                         ['Top Bullet', 'Top Bullet Votes'] +
+                         ['Top Pair', 'Top Pair Votes'] + fieldnames[1:])
+            writer = csv.writer(sys.stdout)
+            writer.writerow(outfields)
+
+            currentward = None
+            wardrow = None
+            data = {
+                'type': 'FeatureCollection',
+                'features': []
+            }
+
+            # Start building up the list of data
+            for row in reader:
+                # Get rid of any rows that have an empty first cell. This
+                # includes:
+                # - empty rows
+                # - the totals row at the end
+                if not row[0]:
+                    continue
+
+                # Replace zeros with nothing.
+                row = ['' if v == '0' else v
+                       for v in row]
+
+                # Get rid of spaces in the first column and normalize the
+                # division string.
+                row[0] = row[0].replace(' ', '')
+                ward, division = row[0].split('-')
+
+                # Ward 13 Division 14 in the original spreadsheet had a
+                # backslash (\) in the 0 Votes column.
+                row = [0 if v == '\\' else v for v in row]
+
+                # Keep a running total for each column in the ward.
+                if wardrow is None:
+                    wardrow = row[1:]
+                else:
+                    wardrow = [int(sumv or 0) + int(v or 0)
+                               for sumv, v in zip(wardrow, row[1:])]
+
+                # After we've reached the last row in the ward, calculate the
+                # top bullets and pairs.
+                if currentward != ward:
+                    if currentward is not None:
+                        feature = finalizeward(currentward, wardrow, fieldnames,
+                                               bulletindex, doubleindex, writer,
+                                               wards, outfields)
+                        data['features'].append(feature)
+
+                    # Clear the wardrow for the next iteration.
+                    currentward = ward
+                    wardrow = None
+
+            # Calculate the top bullets and pairs for the last ward.
+            feature = finalizeward(currentward, wardrow, fieldnames,
+                                   bulletindex, doubleindex, writer,
+                                   wards, outfields)
+            data['features'].append(feature)
+
+            # Save the geojson file
+            with open(pathjoin(ROOT, 'data', 'dem_processed_wards.geojson'), 'w') as jsonfile:
+                json.dump(data, jsonfile)
+
+
+if __name__ == '__main__':
+    process_csvs()

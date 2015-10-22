345e0cccdfc84ec0cba9e3837fa9020edc0b42da
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-10-22 02:46:08 -0400

    Add some styles for party menu

--- js/main.js
+++ js/main.js
@@ -44,27 +44,8 @@ var BulletVotes = BulletVotes || {};
 
   var ybinterp = d3.interpolateRgb('#F2BA13', '#027EA4');
 
-  // NOTE: Hard-coding these cadidate values is not ideal, but is easiest for
-  // now.
-  NS.bulletFieldNames = [
-    'neilson', 'rizzo', 'wyatt', 'gym', 'domb', 'green', 'steinke',
-    'reynolds_brown', 'goode', 'aument_loughrey', 'cohen', 'alexander',
-    'thomas', 'greenlee', 'cain', 'ayers', 'write_in', 'other'
-  ];
-  NS.bulletFieldLabels = _.object(NS.bulletFieldNames, [
-    'Neilson', 'Rizzo', 'Wyatt', 'Gym', 'Domb', 'Green', 'Steinke',
-    'Reynolds Brown', 'Goode', 'Aument-Loughrey', 'Cohen', 'Alexander',
-    'Thomas', 'Greenlee', 'Cain', 'Ayers', 'Write in', 'Other'
-  ]);
-  NS.bulletFieldColors = {
-    'green': '#229A00',
-    'neilson': '#E31A1C',
-    'domb': '#1F78B4',
-    'gym': '#7B00B4',
-    'thomas': '#E3E31A',
-    'reynolds_brown': '#FF7F00'
-  };//_.object(NS.bulletFieldNames, [])
-
+  // Populate the candiadate name colors that aren't explicitly set with values
+  // between the alpha.phila.gov yellow and blue.
   (function _fillInNameColors() {
     var unsetNames = _(NS.bulletFieldNames).filter(function(fieldname) { return !NS.bulletFieldColors[fieldname]; })
     var factor = 0;
@@ -74,25 +55,6 @@ var BulletVotes = BulletVotes || {};
       factor += step;
     });
   })();
-
-  NS.pairFieldNames = [
-    'wyatt_neilson', 'green_greenlee', 'green_domb', 'domb_rizzo', 'greenlee_neilson', 'neilson_rizzo', 'domb_gym', 'wyatt_greenlee', 'greenlee_gym', 'wyatt_cohen', 'green_rizzo', 'steinke_rizzo', 'steinke_goode', 'domb_steinke', 'green_wyatt', 'cohen_gym', 'reynolds_brown_neilson', 'cohen_thomas', 'wyatt_rizzo', 'domb_neilson', 'wyatt_gym', 'domb_wyatt',
-    'gym_thomas', 'reynolds_brown_steinke', 'wyatt_steinke', 'green_gym', 'steinke_gym', 'ayers_gym', 'domb_reynolds_brown', 'green_aument_loughrey', 'reynolds_brown_cohen', 'gym_neilson', 'green_neilson', 'neilson_goode', 'domb_aument_loughrey', 'steinke_neilson', 'green_ayers', 'aument_loughrey_gym', 'green_goode', 'steinke_cohen', 'reynolds_brown_goode', 'cohen_neilson', 'rizzo_goode', 'gym_goode',
-    'thomas_goode', 'greenlee_rizzo', 'domb_greenlee', 'ayers_reynolds_brown', 'reynolds_brown_gym', 'ayers_aument_loughrey', 'reynolds_brown_thomas', 'wyatt_reynolds_brown', 'green_cohen', 'aument_loughrey_greenlee', 'cohen_cain', 'gym_rizzo', 'ayers_wyatt', 'alexander_cain', 'ayers_domb', 'reynolds_brown_rizzo', 'cain_aument_loughrey', 'reynolds_brown_greenlee', 'greenlee_goode', 'cain_reynolds_brown', 'ayers_cain', 'ayers_steinke',
-    'green_thomas', 'aument_loughrey_rizzo', 'cohen_goode', 'green_cain', 'cohen_rizzo', 'domb_cohen', 'neilson_thomas', 'domb_thomas', 'wyatt_thomas', 'cain_gym', 'cain_rizzo', 'ayers_cohen', 'alexander_thomas', 'rizzo_thomas', 'ayers_alexander', 'cohen_greenlee', 'steinke_greenlee', 'cain_thomas', 'alexander_rizzo', 'reynolds_brown_aument_loughrey', 'cain_goode', 'aument_loughrey_neilson',
-    'alexander_gym', 'steinke_aument_loughrey', 'cain_steinke', 'alexander_wyatt', 'aument_loughrey_thomas', 'greenlee_thomas', 'ayers_thomas', 'alexander_neilson', 'alexander_cohen', 'ayers_neilson', 'aument_loughrey_cohen', 'domb_cain', 'wyatt_goode', 'cain_greenlee', 'wyatt_cain', 'alexander_goode', 'alexander_reynolds_brown', 'ayers_goode', 'reynolds_brown_green', 'gym_write_in', 'ayers_greenlee', 'cain_cohen',
-    'alexander_steinke', 'wyatt_aument_loughrey', 'cain_neilson', 'rizzo_write_in', 'alexander_greenlee', 'alexander_aument_loughrey', 'thomas_write_in', 'green_reynolds_brown', 'green_alexander', 'green_steinke', 'ayers_rizzo', 'alexander_domb', 'domb_goode', 'steinke_thomas', 'aument_loughrey_goode', 'alexander_write_in',
-    'other'
-  ];
-  NS.pairFieldLabels = _.object(NS.pairFieldNames, [
-    'Wyatt & Neilson', 'Green & Greenlee', 'Green & Domb', 'Domb & Rizzo', 'Greenlee & Neilson', 'Neilson & Rizzo', 'Domb & Gym', 'Wyatt & Greenlee', 'Greenlee & Gym', 'Wyatt & Cohen', 'Green & Rizzo', 'Steinke & Rizzo', 'Steinke & Goode', 'Domb & Steinke', 'Green & Wyatt', 'Cohen & Gym', 'Reynolds-Brown & Neilson', 'Cohen & Thomas', 'Wyatt & Rizzo', 'Domb & Neilson', 'Wyatt & Gym', 'Domb & Wyatt',
-    'Gym & Thomas', 'Reynolds-Brown & Steinke', 'Wyatt & Steinke', 'Green & Gym', 'Steinke & Gym', 'Ayers & Gym', 'Domb & Reynolds-Brown', 'Green & Aument-Loughrey', 'Reynolds-Brown & Cohen', 'Gym & Neilson', 'Green & Neilson', 'Neilson & Goode', 'Domb & Aument & Loughrey', 'Steinke & Neilson', 'Green & Ayers', 'Aument-Loughrey & Gym', 'Green & Goode', 'Steinke & Cohen', 'Reynolds-Brown & Goode', 'Cohen & Neilson', 'Rizzo & Goode', 'Gym & Goode',
-    'Thomas & Goode', 'Greenlee & Rizzo', 'Domb & Greenlee', 'Ayers & Reynolds-Brown', 'Reynolds-Brown & Gym', 'Ayers & Aument-Loughrey', 'Reynolds-Brown & Thomas', 'Wyatt & Reynolds-Brown', 'Green & Cohen', 'Aument-Loughrey & Greenlee', 'Cohen & Cain', 'Gym & Rizzo', 'Ayers & Wyatt', 'Alexander & Cain', 'Ayers & Domb', 'Reynolds-Brown & Rizzo', 'Cain & Aument-Loughrey', 'Reynolds-Brown & Greenlee', 'Greenlee & Goode', 'Cain & Reynolds-Brown', 'Ayers & Cain', 'Ayers & Steinke',
-    'Green & Thomas', 'Aument-Loughrey & Rizzo', 'Cohen & Goode', 'Green & Cain', 'Cohen & Rizzo', 'Domb & Cohen', 'Neilson & Thomas', 'Domb & Thomas', 'Wyatt & Thomas', 'Cain & Gym', 'Cain & Rizzo', 'Ayers & Cohen', 'Alexander & Thomas', 'Rizzo & Thomas', 'Ayers & Alexander', 'Cohen & Greenlee', 'Steinke & Greenlee', 'Cain & Thomas', 'Alexander & Rizzo', 'Reynolds-Brown & Aument-Loughrey', 'Cain & Goode', 'Aument-Loughrey & Neilson',
-    'Alexander & Gym', 'Steinke & Aument-Loughrey', 'Cain & Steinke', 'Alexander & Wyatt', 'Aument-Loughrey & Thomas', 'Greenlee & Thomas', 'Ayers & Thomas', 'Alexander & Neilson', 'Alexander & Cohen', 'Ayers & Neilson', 'Aument-Loughrey & Cohen', 'Domb & Cain', 'Wyatt & Goode', 'Cain & Greenlee', 'Wyatt & Cain', 'Alexander & Goode', 'Alexander & Reynolds-Brown', 'Ayers & Goode', 'Reynolds-Brown & Green', 'Gym & Write-in', 'Ayers & Greenlee', 'Cain & Cohen',
-    'Alexander & Steinke', 'Wyatt & Aument-Loughrey', 'Cain & Neilson', 'Rizzo & Write-in', 'Alexander & Greenlee', 'Alexander & Aument-Loughrey', 'Thomas/Write & In', 'Green & Reynolds-Brown', 'Green & Alexander', 'Green & Steinke', 'Ayers & Rizzo', 'Alexander & Domb', 'Domb & Goode', 'Steinke & Thomas', 'Aument-Loughrey & Goode', 'Alexander & Write-In',
-    'Other'
-  ]);
 
   NS.initWardVotesChart = function() {
     NS.wardVotesChart = c3.generate({
@@ -362,12 +324,17 @@ var BulletVotes = BulletVotes || {};
   };
 
   NS.updateWardBulletsChart = function(values) {
+    // Make sure all the vote count values at least have zeros.
+    values.map(function(v) { v[1] = (!!v[1] ? v[1] : 0); });
+
     // Sort the values. The highest 5 values will be displayed. The remaining
     // values will be collected into an 'other' category.
     var sorted = _(values).sortBy(1).reverse();
     var highest = sorted.slice(0, 5);
-    var lowest = sorted.slice(5);
+    var lowest = sorted.slice(5).concat(_(highest).where({1: 0}));
     var other = lowest.reduce(function(s, v) { return ['other', s[1] + v[1]]; });
+
+    highest = _(highest).filter(function(v) { return !!v[1]; });
 
     NS.updateWardBulletsLegend(highest, lowest);
 
@@ -390,12 +357,17 @@ var BulletVotes = BulletVotes || {};
   };
 
   NS.updateWardPairsChart = function(values) {
+    // Make sure all the vote count values at least have zeros.
+    values.map(function(v) { v[1] = (!!v[1] ? v[1] : 0); });
+
     // Sort the values. The highest 5 values will be displayed. The remaining
     // values will be collected into an 'other' category.
     var sorted = _(values).sortBy(1).reverse();
     var highest = sorted.slice(0, 5);
-    var lowest = sorted.slice(5);
+    var lowest = sorted.slice(5).concat(_(highest).where({1: 0}));
     var other = lowest.reduce(function(s, v) { return ['other', s[1] + v[1]]; });
+
+    highest = _(highest).filter(function(v) { return !!v[1]; });
 
     NS.updateWardPairsLegend(highest, lowest);
 
